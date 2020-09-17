c53754dd84c65c5ae53ecdbe4b64d7831a9910ab
attiliogiglio <attilio.g93@gmail.com>
2020-09-17 15:53:43 -0300

    adding  API Clarifai

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
@@ -6,21 +6,34 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Logo from './Components/Logo/Logo';
 import Navigation from './Components/Navigation/Navigation';
 import Rank from './Components/Rank/Rank';
+import Clarifai from 'clarifai';
 
 function App() {
 
   const app = new Clarifai.App({
-    apiKey: '7201138936c0423db4b982e7484e0e98'
-   });
+    apiKey: '830df3e3c47a4135989c97e73c30b3d9'
+  });
 
-  const [state, setState] = useState({input:''})
+  const [state, setState] = useState({ 
+    input: '',
+    imageUrl: '' 
+  })
 
   const onInputChange = (e) => {
-   console.log(e.target.value) 
+    console.log(e.target.value)
   }
 
   const onButtonChange = (e) => {
-console.log('click')
+    setState({imageUrl:input})
+    app.models.predict(
+      Clarifai.COLOR_MODEL, 
+      'https://samples.clarifai.com/metro-north.jpg')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        // There was an error
+      });
   }
 
   const particlesOptions = {
@@ -34,6 +47,9 @@ console.log('click')
       }
     }
   }
+
+  const {input, imageUrl} = state;
+  
   return (
     <div className="App">
       <Particles className='particles'
@@ -42,8 +58,12 @@ console.log('click')
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm onInputChange={onInputChange} onButtonChange={onButtonChange}/>
-      <FaceRecognition />
+      <ImageLinkForm 
+      onInputChange={onInputChange} 
+      onButtonChange={onButtonChange} />
+      <FaceRecognition 
+      imageUrl={imageUrl}
+      />
     </div>
   );
 }
