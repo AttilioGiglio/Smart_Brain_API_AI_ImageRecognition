7433666c0d86a4c724816a67adfb8c93c2364dc5
attiliogiglio <attilio.g93@gmail.com>
2020-09-17 12:23:49 -0300

    adding animation icons library, called Tilt from npm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import Logo from './Components/Logo/Logo';
 import Navigation from './Components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
